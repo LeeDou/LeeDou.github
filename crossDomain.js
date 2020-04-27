1615b932b00cf94fe14589545950daa526852b60
li kang <likang@sensorsdata.cn>
2020-04-27 17:58:56 +0800

    add

--- crossDomain.js
+++ crossDomain.js
@@ -120,8 +120,8 @@ crossDomain.isSameDomain = function(url) {
 crossDomain.getUrlId = function() {
   var that = this;
   var location = document.location,
-      search = location.search.split('?')[1],
-      hash = location.hash.split('?')[1];
+      search = location.search.split('?')[1] || '',
+      hash = location.hash.split('?')[1] || '';
   var searchId = that.getUrlValue(search, '_sa_sdk') || '',
       hashId = that.getUrlValue(hash, '_sa_sdk') || '';
   return searchId ? decodeURIComponent(searchId) : decodeURIComponent(hashId);
